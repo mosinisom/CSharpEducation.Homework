3a80e8b942900b79ac5a062dc257b211b2eb567c
alex mosin <alexmosin@inbox.ru>
2024-08-19 12:48:32 +0400

    Добавьте файлы проекта.

---
+++ Homeworks/Homeworks.Phonebook/Abonent.cs
@@ -0,0 +1,12 @@
+namespace phonebook;
+internal class Abonent
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public Abonent(string name, string phoneNumber)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+    }
+}
---
+++ Homeworks/Homeworks.Phonebook/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace phonebook;
+using System;
+class Program
+{
+    static void Main()
+    {
+        // тут мог быть пример работы с Phonebook, но в тз его нет, значит и тут его нет
+    }
+}
